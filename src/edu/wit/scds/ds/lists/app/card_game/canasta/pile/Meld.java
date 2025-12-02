aa41cc2e7a123c8b1001f12d6148bca571e75969
lynns <snowl1@wit.edu>
2025-12-02 12:55:35 -0500

    Now validates canastas

--- src/edu/wit/scds/ds/lists/app/card_game/canasta/pile/Meld.java
+++ src/edu/wit/scds/ds/lists/app/card_game/canasta/pile/Meld.java
@@ -32,7 +32,12 @@ package edu.wit.scds.ds.lists.app.card_game.canasta.pile ;
 
 import static edu.wit.scds.ds.lists.app.card_game.universal_base.support.Orientation.FACE_UP ;
 
+import edu.wit.scds.ds.lists.app.card_game.standard_cards.card.Card;
+import edu.wit.scds.ds.lists.app.card_game.standard_cards.card.Rank;
+import edu.wit.scds.ds.lists.app.card_game.standard_cards.card.Suit;
+import edu.wit.scds.ds.lists.app.card_game.standard_cards.pile.Deck;
 import edu.wit.scds.ds.lists.app.card_game.standard_cards.pile.Pile ;
+import edu.wit.scds.ds.lists.app.card_game.universal_base.card.CardBase;
 import edu.wit.scds.ds.lists.app.card_game.universal_base.support.Orientation ;
 
 /**
@@ -45,9 +50,9 @@ import edu.wit.scds.ds.lists.app.card_game.universal_base.support.Orientation ;
  *
  * @version 1.0 2025-06-28 Initial implementation (taken from {@code Field}
  * 
- * @author Your Name
+ * @author Lynn Snow
  * 
- * @version 2.0 2025-11-03 only modify this tag if you modified the code
+ * @version 2.0 2025-12-01 canasta implementation
  */
 public final class Meld extends Pile
     {
@@ -88,6 +93,11 @@ public final class Meld extends Pile
 
         super.moveCardsToBottom( cardsToadd ) ;
 
+        if (!validateMeld())
+            {
+            super.removeAll() ;
+            System.out.println("Not a valid canasta.") ;
+            }
         }   // end 1-arg constructor
 
 
@@ -104,8 +114,155 @@ public final class Meld extends Pile
      */
     public static void main( final String[] args )
         {
-        // TODO Auto-generated method stub
+        final Stock myStock = new Stock() ;     // should only accept permanent cards
+        final Deck cardSource = new Deck() ;    // contains only permanent cards
+
+        myStock.moveCardsToBottom( cardSource ) ; // I'm genuinely so pissed
+
+        myStock.sort() ;
+
+        System.out.println(myStock.revealAll());
+
+        DiscardPile discardPile = new DiscardPile() ;
+
+        discardPile.addCard(myStock.removeCardAt(5));
+        discardPile.addToBottom(myStock.removeCardAt(5));
+        discardPile.addToBottom(myStock.removeCardAt(5));
+        discardPile.addToBottom(myStock.removeCardAt(5));
+        discardPile.addToBottom(myStock.removeCardAt(5));
+        //discardPile.addToBottom(myStock.removeCardAt(5));
+
+        Meld meld = new Meld(discardPile) ;
+
 
         }	// end main()
+
+    /**
+     * Verifies the meld that the user creates by making sure they are all
+     * three of the same card
+     */
+
+    private boolean validateMeld()
+        {
+        int wildCardCounter = 0 ;
+        Card currentCard;
+        Card previousCard = null ;
+        Card constantCard = null ;
+
+        for (CardBase card : cards) {
+
+            currentCard = (Card) card ;
+
+        // If current card is joker or 2 add to the count
+        if ( currentCard.rank == Rank.JOKER || currentCard.rank == Rank.TWO )
+            {
+            if (wildCardCounter == 3)
+                {
+                return false;
+                }
+            else
+                {
+                previousCard = currentCard ;
+                wildCardCounter++ ;
+                continue ;
+                }
+            }
+
+        // Continue if previous card is null
+        if ( previousCard == null )
+            {
+            previousCard = currentCard ;
+            continue ;
+            }
+
+        // Continue if previous card is a wildcard
+        if ( previousCard.rank == Rank.JOKER || previousCard.rank == Rank.TWO )
+            {
+            previousCard = currentCard ;
+            continue ;
+            }
+
+        // At this point we've asserted that the previous card isn't a wildcard or null,
+        // and that the current card is not a wildcard
+
+        // Sets the constant card to current card if constant is null
+        if (constantCard == null)
+            {
+            constantCard = currentCard ;
+            previousCard = currentCard ;
+            continue ;
+            }
+
+        // If the constant card's rank does not match the current card's rank, not a valid meld
+        if (!constantCard.rank.equals(currentCard.rank))
+            {
+            return false ;
+            }
+
+        previousCard = currentCard ;
+
+        }
+
+
+
+/*
+        while (this.iterator().hasNext())
+            {
+            // Check each card in the meld
+            currentCard = (Card) this.iterator().next() ;
+            System.out.println(currentCard.toString()) ;*/
+
+            // If current card is joker/2 add to the count
+/*            if ( currentCard.rank == Rank.JOKER || currentCard.rank == Rank.TWO )
+                {
+                if (wildCardCounter == 3)
+                    {
+                    return false;
+                    }
+                else
+                    {
+                    previousCard = currentCard ;
+                    wildCardCounter++ ;
+                    continue ;
+                    }
+                }
+
+            // Continue if previous card is null
+            if ( previousCard == null )
+                {
+                previousCard = currentCard ;
+                continue ;
+                }
+
+            // Continue if previous card is a wildcard
+            if ( previousCard.rank == Rank.JOKER || previousCard.rank == Rank.TWO )
+                {
+                previousCard = currentCard ;
+                continue ;
+                }
+
+            // At this point we've asserted that the previous card isn't a wildcard or null,
+            // and that the current card is not a wildcard
+
+            // Sets the constant card to current card if constant is null
+            if (constantCard == null)
+                {
+                constantCard = currentCard ;
+                previousCard = currentCard ;
+                continue ;
+                }
+
+            // If the constant card's rank does not match the current card's rank, not a valid meld
+            if (!constantCard.rank.equals(currentCard.rank))
+                {
+                return false ;
+                }
+
+            previousCard = currentCard ;
+            }*/
+
+        return true ;
+
+        } // end validateMeld()
 
     }	// end class Meld
